49ab71feb096b44d8cc591d1df165cd8c8a26c32
MICHAELSCOFIELD-95 <152068890+MICHAELSCOFIELD-95@users.noreply.github.com>
2026-07-06 00:14:31 +0530

    Added HTML report generation

--- Jenkinsfile
+++ Jenkinsfile
@@ -14,4 +14,28 @@ pipeline {
             }
         }
     }
+    pipeline {
+    agent any
+
+    stages {
+
+        stage('Build Docker Image') {
+            steps {
+                bat 'docker build -t log-monitor .'
+            }
+        }
+
+        stage('Run Container') {
+            steps {
+                bat 'docker run --rm log-monitor'
+            }
+        }
+
+        stage('Archive HTML Report') {
+            steps {
+                archiveArtifacts artifacts: 'reports/report.html', fingerprint: true
+            }
+        }
+    }
+}
 }
--- analyzer.py
+++ analyzer.py
@@ -1,4 +1,5 @@
 from collections import Counter
+import os
 
 log_file = "logs/application.log"
 
@@ -28,4 +29,97 @@ for error, count in error_types.items():
 print("\nDetected Errors:")
 for error in error_logs:
     print(error)
-# Webhook Test
+
+# -------------------------
+# Generate HTML Report
+# -------------------------
+
+os.makedirs("reports", exist_ok=True)
+
+html = f"""
+<!DOCTYPE html>
+<html>
+<head>
+<title>DevOps Log Report</title>
+
+<style>
+body {{
+    font-family: Arial;
+    background: #f4f4f4;
+    padding: 40px;
+}}
+
+.container {{
+    background: white;
+    padding: 30px;
+    border-radius: 10px;
+}}
+
+h1 {{
+    color: #2c3e50;
+}}
+
+table {{
+    border-collapse: collapse;
+    width: 50%;
+}}
+
+th, td {{
+    border: 1px solid black;
+    padding: 10px;
+}}
+
+th {{
+    background: #3498db;
+    color: white;
+}}
+</style>
+
+</head>
+
+<body>
+
+<div class="container">
+
+<h1>DevOps Log Analysis Report</h1>
+
+<h2>Summary</h2>
+
+<p><b>Total Logs:</b> {total_logs}</p>
+<p><b>Total Errors:</b> {len(error_logs)}</p>
+
+<h2>Error Summary</h2>
+
+<table>
+<tr>
+<th>Error Type</th>
+<th>Count</th>
+</tr>
+"""
+
+for error, count in error_types.items():
+    html += f"<tr><td>{error}</td><td>{count}</td></tr>"
+
+html += """
+</table>
+
+<h2>Detected Errors</h2>
+<ul>
+"""
+
+for err in error_logs:
+    html += f"<li>{err}</li>"
+
+html += """
+</ul>
+
+</div>
+
+</body>
+</html>
+"""
+
+with open("reports/report.html", "w") as file:
+    file.write(html)
+
+print("\nHTML report generated successfully!")
